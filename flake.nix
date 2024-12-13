8253ab782794000cce02b87fb7c02a18518bfdab
Joshua Sims <jm8hjkl@gmail.com>
2024-12-13 18:14:50 -0500

    leriagtjrgjaslj

--- flake.nix
+++ flake.nix
@@ -4,26 +4,34 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nixgl.url = "github:nix-community/nixGL";
   };
 
   outputs = {
     self,
     nixpkgs,
+    nixgl,
     flake-utils,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
-        pkgs = import nixpkgs {
-          inherit system;
+        godot_wrap_overlay = final: prev: {
+          godot_4 = pkgs.writeShellScriptBin "godot4" ''
+            exec ${prev.nixgl.auto.nixGLDefault}/bin/nixGL ${prev.godot_4}/bin/godot4 "$@"
+          '';
         };
-      in
-        {
-          devShells.default = pkgs.mkShell {
-            nativeBuildInputs = [
-              pkgs.godot_4
-            ];
-          };
-        }
+        overlays = [nixgl.overlay godot_wrap_overlay];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.nixgl.auto.nixGLDefault
+            pkgs.godot_4
+          ];
+        };
+      }
     );
 }
