57b9087b00f43c50043b5f41c463bdc5bb472385
Joshua Sims <jm8hjkl@gmail.com>
2024-12-13 17:26:04 -0500

    temp

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A devShell example";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+        {
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = [
+              pkgs.godot_4
+            ];
+          };
+        }
+    );
+}
