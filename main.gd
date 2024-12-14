6d1b8edb6ef9695cbbce3dd4a6e50d852b41631e
Joshua Sims <jm8hjkl@gmail.com>
2024-12-13 20:53:29 -0500

    Outline selected hex

--- main.gd
+++ main.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
-
 func _ready() -> void:
 	print(Globulars.test)
+
+func _process(_delta: float) -> void:
+	var mouse = $TileMapLayer.get_local_mouse_position()
+	var moose = $TileMapLayer.local_to_map(mouse)
+	$SelectedHexagon.global_position = $TileMapLayer.global_position + $TileMapLayer.map_to_local(moose)
