7b9363e742a5f1010bc39003fb2c24f348b890af
Joshua Sims <jm8hjkl@gmail.com>
2024-12-13 23:27:48 -0500

    temp (add buttons from the resournce)

--- ui.gd
+++ ui.gd
@@ -1,11 +1,30 @@
 extends Node2D
 
-@export var tower_types: Array[TowerType]
+@onready var buttons = %TowerButtonsContainer
+@onready var tower_description = %TowerDescription
+var CreateTowerButton = preload("res://create_tower_button.tscn")
 
-@onready var build_tower_buttons = $ScreenSpace/Control/MarginContainer/BuildTowerButtons
+func create_buttons():
+	for child in buttons.get_children():
+		child.queue_free()
+	for tower_type in tower_types:
+		var button: Button = CreateTowerButton.instantiate()
+		button.icon = tower_type.texture
+		button.tooltip_text = tower_type.name
+		buttons.add_child(button)
+		button.mouse_entered.connect(func():
+			tower_description.text = tower_type.description
+		)
+		button.pressed.connect(func():
+			print(tower_type.name)
+		)
+
+
+@export var tower_types: Array[TowerType]:
+	set(value):
+		tower_types = value
+		if buttons != null:
+			create_buttons()
 
 func _ready() -> void:
-	pass # Replace with function body.
-
-func _process(delta: float) -> void:
-	pass
+	create_buttons()
