c7a7a9d740b6853f7879b27c212fe4b8dc8c3bc7
Christopher McDevitt <christophermcd0903@gmail.com>
2024-12-19 11:50:54 -0500

    make fireball tower use heat

--- fireball.gd
+++ fireball.gd
@@ -1,19 +1,17 @@
 extends Sprite2D
 class_name Fireball
 
-var time = 10
+var time = 0.25
 
-func _process(delta: float) -> void:
-	position += Vector2(20, 0).rotated(global_rotation)
+func _physics_process(delta: float) -> void:
+	position += Vector2(2500, 0).rotated(global_rotation) * delta
 	time -= delta
 	if time < 0:
 		queue_free()
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.get_parent() is Enemy:
-		print("HIT")
 		for e in $BlastRadius.get_overlapping_areas():
-			print(e)
 			if e.get_parent() is Enemy:
 				e.get_parent().health -= 20
 		visible = false
--- tower_2.gd
+++ tower_2.gd
@@ -6,27 +6,62 @@ extends Tower
 var cooldown_time: float = 2
 var cooldown_timer: float = cooldown_time
 
+func set_temperature(value):
+	var previous_range = temperature_range(temperature)
+	var next_range = temperature_range(value)
+	if previous_range != next_range:
+		set_bar_style(next_range)
+	return value
+
+enum TemperatureRange {
+	Low,
+	Medium,
+	High
+}
+
+func set_bar_style(range: TemperatureRange):
+	match range:
+		TemperatureRange.Low:
+			$TemperatureBar.add_theme_stylebox_override("fill", Globulars.PROGRESS_LOW)
+		TemperatureRange.Medium:
+			$TemperatureBar.add_theme_stylebox_override("fill", Globulars.PROGRESS_MEDIUM)
+		TemperatureRange.High:
+			$TemperatureBar.add_theme_stylebox_override("fill", Globulars.PROGRESS_HIGH)
+
+func temperature_range(t: float) -> TemperatureRange:
+	var range = TemperatureRange.Low
+	if t > operating_temperature:
+		range = TemperatureRange.Medium
+	if t > min_temperature + (max_temperature - min_temperature) * 0.8:
+		range = TemperatureRange.High
+	return range
+
+func _ready() -> void:
+	$TemperatureBar.min_value = min_temperature
+	$TemperatureBar.max_value = max_temperature
+	set_bar_style(temperature_range(temperature))
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if temperature < min_temperature:
 		delete()
 		return
-
+	
 	$TemperatureBar.value = temperature
-	$TemperatureBar.add_theme_stylebox_override("fill", Globulars.PROGRESS_LOW)
-	if temperature > operating_temperature:
-		$TemperatureBar.add_theme_stylebox_override("fill", Globulars.PROGRESS_MEDIUM)
-	if temperature > min_temperature + (max_temperature - min_temperature) * 0.8:
-		$TemperatureBar.add_theme_stylebox_override("fill", Globulars.PROGRESS_HIGH)
-		
+	
+	if temperature < operating_temperature:
+		return
+	
 	var target: Enemy = get_tree().get_first_node_in_group("targets" + name)
+	
+	temperature -= 20 * (cooldown_timer - max(0, cooldown_timer - delta))
 	cooldown_timer = max(0, cooldown_timer - delta)
 	if cooldown_timer <= 0 and target:
+		temperature -= 10
 		var fireball: Fireball = fireball_scene.instantiate()
 		var rotation: float = global_position.direction_to(target.global_position).angle()
 		fireball.global_rotation = rotation
 		add_child(fireball)
-		print("FIRE!")
 		cooldown_timer = cooldown_time
 	
 
